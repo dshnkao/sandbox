0a366c79809f38c041b0d19ae51efbb15fed0a37
Dennis Kao <dshnkao@gmail.com>
2021-12-21 00:33:00 +1100

    init

---
+++ sandbox-java/src/main/java/com/denniskao/MyAtomicReference.java
@@ -0,0 +1,9 @@
+package com.denniskao;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class MyAtomicReference {
+    public static void main(String[] args) {
+        var x = new AtomicReference<>();
+    }
+}
---
+++ sandbox-java/src/main/java/com/denniskao/MyVarHandle.java
@@ -0,0 +1,19 @@
+package com.denniskao;
+
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
+
+public class MyVarHandle {
+
+    int i;
+
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        var obj = new MyVarHandle();
+        var handle = MethodHandles.lookup().in(MyVarHandle.class)
+                .findVarHandle(MyVarHandle.class, "i", int.class);
+        handle.accessModeType(VarHandle.AccessMode.GET);
+        handle.set(obj, 2);
+        System.out.println(handle.getVolatile(obj));
+        System.out.println(handle.varType());
+    }
+}
