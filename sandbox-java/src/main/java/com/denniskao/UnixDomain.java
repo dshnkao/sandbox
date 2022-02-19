027f9a48d821a67d90266235add7335067741a6f
Dennis Kao <dshnkao@gmail.com>
2022-02-19 23:48:35 +1100

    socket

--- sandbox-java/src/main/java/com/denniskao/UnixDomain.java
+++ sandbox-java/src/main/java/com/denniskao/UnixDomain.java
@@ -2,20 +2,23 @@ package com.denniskao;
 
 import java.io.IOException;
 import java.net.SocketAddress;
+import java.net.SocketOption;
 import java.net.StandardProtocolFamily;
 import java.net.UnixDomainSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
 
 public class UnixDomain {
     public static void main(String[] args) throws IOException {
-        var acceptor = ServerSocketChannel.open(StandardProtocolFamily.UNIX)
-                .socket()
-                .bind(UnixDomainSocketAddress.of("./abc"));
+        var server = ServerSocketChannel.open(StandardProtocolFamily.UNIX);
+        var address = UnixDomainSocketAddress.of("./abc.socket");
+        server.bind(address);
+
 
         while (true) {
-            var channel = acceptor.accept();
+            var channel = server.accept();
             var buf = ByteBuffer.allocate(100);
             channel.read(buf);
             System.out.println(StandardCharsets.UTF_8.decode(buf.flip()));
