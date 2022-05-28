08f6bd8a1f79359e9e308ba71fe1c98457903e9f
Dennis Kao <dennis.k@canva.com>
2022-05-28 12:01:10 +1000

    anonclassname

--- sandbox-java/src/main/java/com/denniskao/Lambda.java
+++ sandbox-java/src/main/java/com/denniskao/Lambda.java
@@ -1,2 +1,22 @@
-package com.denniskao;public class Lambda {
+package com.denniskao;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Stream;
+
+public class Lambda {
+    private static final ExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+
+    public static void main(String[] args) {
+
+
+        for (var i = 0; i < 10; i++) {
+            executor.submit(() -> {
+            });
+            executor.submit(() -> {
+            });
+        }
+        Stream.of(Lambda.class.getDeclaredMethods()).forEach(System.out::println);
+        executor.shutdown();
+    }
 }
